24e6d6769a8eacfdb9742906d6fab43080fb2f5d
M Gopal Krishna <96627837+MGopalkrishnaL@users.noreply.github.com>
2023-02-10 16:00:13 +0530

    Update ILibraryServer.java

--- src/main/java/com/example/Library/server/ILibraryServer.java
+++ src/main/java/com/example/Library/server/ILibraryServer.java
@@ -8,7 +8,7 @@ public interface ILibraryServer {
     public Map<String, Library> getAllList();
     public Library getlibByName(String name);
     public String addlib(Library library);
-    public String deletelib(int id);
+    public String deletelib(String id);
 
     public String updatelib(String name, Library lib);
 
